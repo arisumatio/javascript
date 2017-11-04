5af68ec18fa5b8677a2303f662b5f28f6fa412ec
arisumatio <alibabat59@yahoo.com>
2017-11-04 17:23:20 +0700

    Update eonads.js

--- eonads.js
+++ eonads.js
@@ -1,4 +1,2 @@
  document.write('<ins data-revive-zoneid=1107 data-revive-id="f87497be83f'
- +'6ed6b52c3b340d803ae0d"/>
-<script async src="//network.eonads.com/adser'
- +'ver/www/delivery/asyncjs.php"/>');
+ +'6ed6b52c3b340d803ae0d"></ins>');
