b086058a835a6089b05838301a89f9b94246c004
arisumatio <alibabat59@yahoo.com>
2017-11-04 18:06:59 +0700

    Create exoads2.js

---
+++ exoads2.js
@@ -0,0 +1,3 @@
+ document.write('<iframe src="//ads.exdynsrv.com/iframe.php?idzone=282545'
+ +'6&size=728x90" width="728" height="90" scrolling="no" marginwidth="0" '
+ +'marginheight="0" frameborder="0"></iframe>');
