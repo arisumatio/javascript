700449d6e41c4d0dd5f28e96890034e725fa81aa
arisumatio <alibabat59@yahoo.com>
2017-12-11 16:50:26 +0700

    Create tokenad.js

---
+++ tokenad.js
@@ -0,0 +1,5 @@
+ document.write('<script type="text/javascript">var SC_CId = "447364",SC_'
+ +'Domain="n.tckn-code.com";SC_Start_447364=(new Date).getTime();</script'
+ +'>
+<script type="text/javascript" src="//st-n.tckn-code.com/js/adv_out.'
+ +'js"></script>');
